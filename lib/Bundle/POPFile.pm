99f10350dd40ff809732734a60b88036eb39c79e
Manni Heumann <cpan@lxxi.org>
2008-08-21 15:43:30 +0000

    Import of MANNI/Bundle-POPFile-1.1.0 from CPAN.
    gitpan-cpan-distribution: Bundle-POPFile
    gitpan-cpan-version:      1.1.0
    gitpan-cpan-path:         MANNI/Bundle-POPFile-1.1.0.tar.gz
    gitpan-cpan-author:       MANNI
    gitpan-cpan-maturity:     released

--- lib/Bundle/POPFile.pm
+++ lib/Bundle/POPFile.pm
@@ -1,15 +1,18 @@
 package Bundle::POPFile;
 
-$VERSION = '1.02';
-
-
+$VERSION = '1.1.0';
 
 1;
+
 __END__
 
 =head1 NAME
 
 Bundle::POPFile - The modules needed by POPFile in one clean bundle
+
+=head1 VERSION
+
+This is version 1.1.0 of the bundle for POPFile 1.1.0
 
 =head1 SYNOPSIS
 
@@ -18,12 +21,9 @@ C<perl -MCPAN -e 'install Bundle::POPFile'>
 =head1 DESCRIPTION
 
 This bundle will give you all the module dependencies of POPFile L<http://getpopfile.org/>
-with a single cpan install command. Please note that no modules for the database
-backend are included, except L<DBI>.
+with a single cpan install command.
 
 =head1 CONTENTS
-
-BerkeleyDB
 
 DBI
 
@@ -33,9 +33,7 @@ Date::Parse
 
 Digest::MD5
 
-Encode
-
-Encode::Guess
+Fcntl
 
 File::Copy
 
@@ -55,21 +53,11 @@ IO::Select
 
 IO::Socket
 
-IO::Socket::SSL
-
-IO::Socket::Socks
-
 MIME::Base64
 
 MIME::QuotedPrint
 
 Sys::Hostname
-
-Text::Kakasi
-
-XMLRPC::Transport::HTTP
-
-
 
 =head1 AUTHOR
 
@@ -77,10 +65,10 @@ Manni Heumann, E<lt>pfbundle AT lxxi.orgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2007 by Manni Heumann
+Copyright (C) 2008 by Manni Heumann
 
 This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.8.6 or,
+it under the same terms as Perl itself, either Perl version 5.8.8 or,
 at your option, any later version of Perl 5 you may have available.
 
 
